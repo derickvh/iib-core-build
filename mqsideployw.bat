304c66eaf1e5b135cb9e4b20d93023214862cb05
derickvh <dvanheerden@gmail.com>
2016-11-01 16:56:39 +0200

    mqsideployw.bat snafu sorted

--- mqsideployw.bat
+++ mqsideployw.bat
@@ -1,7 +1,23 @@
-#!/bin/sh
+@REM Windows batch file to set the mqsi environment
+@REM and execute the mqsiprofile command.
 
-MQSIPROFILE=$2/mqsiprofile
+@REM Saves the current working directory and enter the directory where 'mqsiprofile' generally lives.
 
-source "$MQSIPROFILE" > /dev/null 2>&1
+@pushd %1
 
-mqsideploy $3 -i $4 -p $5 -a $6 -e $7
+@call "mqsiprofile.cmd" >NUL 2>NUL
+
+:: Get back to the project directory
+
+@popd
+
+:: Now mqsideploy can be called.
+
+::
+:: %2 = integration node
+:: %3 = hostname
+:: %4 = port
+:: %5 = bar file
+:: %6 = intergration server (execution group)
+
+mqsideploy %2 -i %3 -p %4 -a %5 -e %6
